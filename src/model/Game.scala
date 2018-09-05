437aab9317029be7db6b51a149ff0c4e0f0a23cb
Kenneth Leider <ken.leider@canoeventures.com>
2018-09-05 16:57:34 -0600

    Parsing json files.

--- src/model/Game.scala
+++ src/model/Game.scala
@@ -5,62 +5,65 @@ import java.time.Instant
 import java.time.temporal.ChronoUnit
 
 import scala.io.Source
+import scala.util.parsing.json._
 import scala.xml.XML
 
 
 object Game {
   val cacheDir = new File("cache")
   cacheDir.mkdirs()
-
-  val addOns : Set[String] = Source.fromInputStream(classOf[Game].getResourceAsStream("/add-ons.txt")).getLines().flatMap { line =>
+  
+  val addOns: Set[String] = Source.fromInputStream(classOf[Game].getResourceAsStream("/add-ons.txt")).getLines().flatMap { line =>
     line.split(":")(1).split(",")
   }.toSet
-
-  val skip : Map[String, String] = Source.fromInputStream(classOf[Game].getResourceAsStream("/steam/skip.txt")).getLines().map { line =>
+  
+  val skip: Map[String, String] = Source.fromInputStream(classOf[Game].getResourceAsStream("/steam/skip.txt")).getLines().map { line =>
     val pattern = "(.*?):(.*)".r
-
+    
     line.trim match {
       case pattern(key, value) => (key -> value)
       case _ => "" -> ""
     }
   }.toMap
-
-  val replace : Map[String, String] = Source.fromInputStream(classOf[Game].getResourceAsStream("/steam/replace.txt")).getLines().map { line =>
+  
+  val replace: Map[String, String] = Source.fromInputStream(classOf[Game].getResourceAsStream("/steam/replace.txt")).getLines().map { line =>
     val pattern = "(.*?):(.*)".r
-
+    
     line.trim() match {
       case pattern(key, value) => (key -> value)
     }
   }.toMap
-
-  def load(key : String, userID : String) = {
-    val cacheFile = new File(cacheDir, s"user-${userID}.xml")
+  
+  def load(key: String, userID: String) = {
+    val cacheFile = new File(cacheDir, s"user-${userID}.json")
     if (!cacheFile.exists() || Instant.ofEpochMilli(cacheFile.lastModified()).compareTo(Instant.now().minus(2, ChronoUnit.DAYS)) != 1) {
-      val url = s"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${key}&steamid=${userID}&format=xml&include_played_free_games=1&include_appinfo=1"
+      val url = s"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${key}&steamid=${userID}&format=json&include_appinfo=1&include_played_free_games=1"
       val text = Source.fromURL(url).mkString
       val writer = new PrintWriter(cacheFile)
       writer.write(text)
       writer.close()
     }
-
-    val root = XML.loadFile(cacheFile)
-    val games = (root \ "games" \ "message").map { message =>
-      val id = (message \ "appid").text
-      val playtime = (message \ "playtime_forever").text
-      Game(id, playtime.toInt)
-    }.filterNot { game =>
-      addOns.contains(game.appID)
-    }.filterNot { game =>
-      val result = skip.contains(game.appID)
-      if (result) println(s"Skipping ${game.appID}: ${skip(game.appID)}")
-      result
-    }
-
-    val (replacements, keep) = games.partition( game => replace.contains(game.appID) )
+    
+    val json = JSON.parseRaw(Source.fromFile(cacheFile).mkString).get.asInstanceOf[JSONObject]
+    val games = json.obj("response").asInstanceOf[JSONObject].obj("games").asInstanceOf[JSONArray].list
+       .map(_.asInstanceOf[JSONObject].obj)
+       .map(obj => Game(obj("appid").toString, obj("playtime_forever").toString.toDouble.toInt))
+       .filterNot { game => addOns.contains(game.appID) }
+       .filterNot { game =>
+         val result = skip.contains(game.appID)
+         if (result) println(s"Skipping ${game.appID}: ${skip(game.appID)}")
+         result
+       }
+    
+    val (replacements, keep) = games.partition(game => replace.contains(game.appID))
     val timeUpdates = replacements.map { game => game.copy(appID = replace(game.appID)) }
-    val newGames = timeUpdates.filterNot { game => keep.exists {k => k.appID.equals(game.appID)}}.map { game => game.copy(playtimeMinutes = 0)}.toSet
+    val newGames = timeUpdates.filterNot { game => keep.exists { k => k.appID.equals(game.appID) } }.map { game => game.copy(playtimeMinutes = 0) }.toSet
     (keep ++ newGames).map { game =>
-      val extraTime = timeUpdates.filter { _.appID.equals(game.appID)}.map { _.playtimeMinutes }.sum
+      val extraTime = timeUpdates.filter {
+        _.appID.equals(game.appID)
+      }.map {
+        _.playtimeMinutes
+      }.sum
       if (extraTime > 0) println(s"Adding ${extraTime} minutes to ${game.appID}")
       game.copy(playtimeMinutes = game.playtimeMinutes + extraTime)
     }
--- src/steam/SteamScraper.scala
+++ src/steam/SteamScraper.scala
@@ -17,39 +17,42 @@ object SteamScraper {
     val id = args(1)
 
     val games = Game.load(key, id)
-
-    val ownedElseWhere = Source.fromInputStream(classOf[Game].getResourceAsStream("/steam/owned_elsewhere.txt")).getLines().filter { !_.isEmpty }.map { line =>
-      val tokens = line.split(":")
-      val id = tokens(0)
-      val tags = tokens(1)
-      Try(Note(Game(id, 0), tags))
-    }.toSeq
+    
+//    val ownedElseWhere = Source.fromInputStream(classOf[Game].getResourceAsStream("/steam/owned_elsewhere.txt")).getLines().filter { !_.isEmpty }.map { line =>
+//      val tokens = line.split(":")
+//      val id = tokens(0)
+//      val tags = tokens(1)
+//      Try(Note(Game(id, 0), tags))
+//    }.toSeq
 
     //val steamNotes = Seq[Try[Note]]()
-    //val steamNotes = Seq(Try(Note(Game("259530", 0), "steam")))
-    val steamNotes = games.map { game => Try(Note(game, "steam")) }
-
-
-    ownedElseWhere.map { _.map { _.sourceUrl }}.toSet.intersect(steamNotes.map { _.map { _.sourceUrl }}.toSet).foreach { _.map { url =>
-      println(s"Duplicate between owned elsewhere and steam: $url")
-    }}
-    val notes = (ownedElseWhere ++ steamNotes).collect {
-      case Success(note) => note
+    val steamNotes = Seq(Try(Note(Game("259530", 0), "steam")))
+//    val steamNotes = games.map { game => Try(Note(game, "steam")) }
+    steamNotes.collect {
+      case Failure(th) => th.getMessage
+      case Success(note) => note.title
     }
 
-    (ownedElseWhere ++ steamNotes).collect {
-      case Failure(ex) =>
-        println(ex.getMessage + s":${ex.getCause.getMessage}")
-        None
-    }
-
-    notes.foreach { note =>
-      val appID = note.sourceUrl.split("/").reverse.head
-      val writer = new PrintWriter(s"notes/steam/$appID.xml","UTF-16")
-      println (s"Writing notes/steam/$appID.xml")
-      writer.write( new PrettyPrinter(500, 3).format(Note.write(Seq(note))))
-      writer.close()
-    }
+//    ownedElseWhere.map { _.map { _.sourceUrl }}.toSet.intersect(steamNotes.map { _.map { _.sourceUrl }}.toSet).foreach { _.map { url =>
+//      println(s"Duplicate between owned elsewhere and steam: $url")
+//    }}
+//    val notes = (ownedElseWhere ++ steamNotes).collect {
+//      case Success(note) => note
+//    }
+//
+//    (ownedElseWhere ++ steamNotes).collect {
+//      case Failure(ex) =>
+//        println(ex.getMessage + s":${ex.getCause.getMessage}")
+//        None
+//    }
+//
+//    notes.foreach { note =>
+//      val appID = note.sourceUrl.split("/").reverse.head
+//      val writer = new PrintWriter(s"notes/steam/$appID.xml","UTF-16")
+//      println (s"Writing notes/steam/$appID.xml")
+//      writer.write( new PrettyPrinter(500, 3).format(Note.write(Seq(note))))
+//      writer.close()
+//    }
 
   }
 }
